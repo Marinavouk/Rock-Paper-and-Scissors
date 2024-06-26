2aedebe3d47dbdd17720c8367750de925ae3e3e0
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-06-26 14:52:33 +0200

    applications functions on

--- Rock,Paper and Scissors/Application.cpp
+++ Rock,Paper and Scissors/Application.cpp
@@ -1,1 +1,26 @@
 #include "Application.h"
+
+bool Application::Create()
+{
+    return false;
+}
+
+void Application::Destroy()
+{
+}
+
+void Application::Run()
+{
+}
+
+void Application::HandleEvents()
+{
+}
+
+void Application::Update()
+{
+}
+
+void Application::Render()
+{
+}
--- Rock,Paper and Scissors/Application.h
+++ Rock,Paper and Scissors/Application.h
@@ -1,5 +1,30 @@
 #pragma once
+
+#include "Window.h"
+#include "Game.h"
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+
+
+
 class Application
 {
+
+public:
+    Application() {}
+    ~Application() {}
+
+    bool Create();
+    void Destroy();
+    void Run();
+    void HandleEvents();
+    void Update();
+    void Render();
+    void Quit() { running = false; }
+
+private:
+    Window* window;
+    Game* game;
+    bool running;
 };
 
