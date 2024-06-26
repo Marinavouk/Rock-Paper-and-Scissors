27d753e347e67329b17fc1386cab9038134231ef
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-06-26 14:55:00 +0200

    game cpp and h functions

--- Rock,Paper and Scissors/Game.cpp
+++ Rock,Paper and Scissors/Game.cpp
@@ -1,1 +1,18 @@
 #include "Game.h"
+
+bool Game::Create(Application* mainApplication)
+{
+    return false;
+}
+
+void Game::Destroy()
+{
+}
+
+void Game::Update(const float deltaTime)
+{
+}
+
+void Game::Render(SDL_Renderer* renderer)
+{
+}
--- Rock,Paper and Scissors/Game.h
+++ Rock,Paper and Scissors/Game.h
@@ -1,5 +1,25 @@
 #pragma once
+
+
+#include <SDL.h>
+
+// Forward-declaring application to avoid multiple inclusion issue
+class Application;
+
 class Game
 {
-};
+public:
 
+	Game() {}
+	~Game() {}
+
+	bool Create(Application* mainApplication);
+	void Destroy();
+	void Update(const float deltaTime);
+	void Render(SDL_Renderer* renderer);
+
+private:
+
+	Application* application = nullptr;
+	SDL_Texture* background = nullptr;
+};
