f1f34ac16999a3ce847397a88281369743b92f02
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-06-26 15:02:32 +0200

    texturehandker functions

---
+++ Rock,Paper and Scissors/TextureHandler.cpp
@@ -0,0 +1,19 @@
+#include "TextureHandler.h"
+
+bool TextureHandler::Create(SDL_Renderer* SDLRenderer)
+{
+    return false;
+}
+
+void TextureHandler::Destroy()
+{
+}
+
+SDL_Texture* TextureHandler::CreateTexture(const std::string& fileName)
+{
+    return nullptr;
+}
+
+void TextureHandler::DestroyTexture(SDL_Texture* texture)
+{
+}
