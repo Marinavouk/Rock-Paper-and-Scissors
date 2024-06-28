fd560200e3bc3b1502dd121a3f27b96b6616168c
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2024-06-28 12:59:08 +0200

    TexureHandle to upload textures is made

--- Rock,Paper and Scissors/TextureHandler.cpp
+++ Rock,Paper and Scissors/TextureHandler.cpp
@@ -1,19 +1,35 @@
 #include "TextureHandler.h"
+#include <SDL_image.h>
+#include <iostream>
 
 bool TextureHandler::Create(SDL_Renderer* SDLRenderer)
 {
-    return false;
+	renderer = SDLRenderer;
+
+	return true;
 }
 
 void TextureHandler::Destroy()
 {
+	renderer = nullptr;
 }
 
 SDL_Texture* TextureHandler::CreateTexture(const std::string& fileName)
 {
-    return nullptr;
+	SDL_Texture* texture = IMG_LoadTexture(renderer, fileName.c_str());
+
+	if (!texture)
+	{
+		std::cout << "Error: failed to load texture" << std::endl;
+		std::cout << IMG_GetError() << std::endl;
+
+		return nullptr;
+	}
+
+	return texture;
 }
 
 void TextureHandler::DestroyTexture(SDL_Texture* texture)
 {
+	SDL_DestroyTexture(texture);
 }
