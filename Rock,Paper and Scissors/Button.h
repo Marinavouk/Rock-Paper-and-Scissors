fdc9c49fc630bdb31db7cdbe5b47b270e400b857
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-07-22 21:36:16 +0200

    tidy up unnecessary code for now

--- Rock,Paper and Scissors/Button.h
+++ Rock,Paper and Scissors/Button.h
@@ -1,39 +1,28 @@
 #pragma once
-
 #include "Game.h"
 #include <SDL.h>
 
 
 class Button
 {
-
 	const int BUTTON_WIDTH = 300;
 	const int BUTTON_HEIGHT = 100;
-	//const int TOTAL_BUTTONS = 1;
 
 	enum ButtonSprite
 	{
-		BUTTON_SPRITE_MOUSE_OUT = 0,
-		/*BUTTON_SPRITE_MOUSE_OVER_MOTION = 1,
-		BUTTON_SPRITE_MOUSE_DOWN = 2,
-		BUTTON_SPRITE_MOUSE_UP = 3,*/
-		BUTTON_SPRITE_TOTAL = 1
+		BUTTON_SPRITE_MOUSE_OUT = 0
 	};
 
 public:
 	Button();
-	
-
 	void render(SDL_Renderer* renderer);
 	void setLocation(int xPos, int yPos);
 	void setTexture(SDL_Texture* texture);
 	bool setEvent(SDL_Event* mouseEvent);
-
 
 private:
 	SDL_Point mPosition;
 	ButtonSprite mCurrentSprite;
 	SDL_Rect buttonRec;
 	SDL_Texture* texture = nullptr;
-
 };
