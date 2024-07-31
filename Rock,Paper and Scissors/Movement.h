d234fde7eeee5b5de45c90a39cc52c46ac340a65
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-07-31 14:56:29 +0200

    header for the movement of the rock, paper and scissors

--- Rock,Paper and Scissors/Movement.h
+++ Rock,Paper and Scissors/Movement.h
@@ -1,5 +1,19 @@
 #pragma once
+#include"SDL.h"
 class Movement
 {
+public:
+	Movement(SDL_Texture* texture, SDL_Rect rect, float velocity, float velocityY, int screenWidth, int screenHeight);
+	void Render(SDL_Renderer* renderer);
+	void Update(float deltaTime);
+
+private:
+	SDL_Texture* texture;
+	SDL_Rect rect;
+	float velocityX;
+	float velocityY;
+	int screenWidth;
+	int screenHeight;
+
 };
 
