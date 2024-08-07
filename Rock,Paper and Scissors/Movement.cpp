6b93fb9bd565a692bbfad6868833829ec105a057
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-08-07 13:46:56 +0200

    images bouncing on the edges

--- Rock,Paper and Scissors/Movement.cpp
+++ Rock,Paper and Scissors/Movement.cpp
@@ -9,3 +9,25 @@ Movement::Movement(SDL_Texture* texture, int screenHeight, int screenWidth, int 
 	texture = texture;
 	rect = { x, y, h, w };
 }
+
+
+
+void Movement::Update(float deltaTime)
+{
+    rect.x += static_cast<int>(velocityX * deltaTime);
+    rect.y += static_cast<int>(velocityY * deltaTime);
+
+    if (rect.x <= 0 || rect.x + rect.w >= screenWidth)
+    {
+        velocityX = -velocityX;
+    }
+    if (rect.y <= 0 || rect.y + rect.h >= screenHeight)
+    {
+        velocityY = -velocityY;
+    }
+}
+
+void Movement::Render(SDL_Renderer* renderer)
+{
+    SDL_RenderCopy(renderer, texture, nullptr, &rect);
+}
